04baa06b92f01d2c072811af19713dd3a1e1c642
Prathapgithub1 <104608530+Prathapgithub1@users.noreply.github.com>
2022-09-04 00:25:39 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,23 @@
+var str1="    my self prathap iam from mancherial and i have dont  my graduatin recently    "
+console.log(str1.length)
+console.log(str1.charAt(0))
+console.log(str1.charAt(0))
+console.log(str1.charAt(1))
+console.log(str1.charAt(2))
+console.log(str1.charAt(2))
+console.log(str1.charAt(3))
+console.log(str1.charAt(4))
+console.log(str1.charAt(5))
+console.log(str1.charAt(6))
+console.log(str1.charAt(7))
+
+console.log(str1.toUpperCase(''))
+console.log(str1.toLowerCase(' '))
+
+console.log(str1.split("s"))
+console.log(str1.trim())
+console.log(str1.trim().length)
+
+console.log(str1.replace("prathap","rana kunarapu"))
+
+console.log(str1.includes('prathap'))//if substring is there in actual string then it will show the true
